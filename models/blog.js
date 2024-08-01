0f97f479fda2c5c546bae1175070ebbb07376bc0
HARSHIT BALODI <balodiharshit1907@gmail.com>
2024-08-02 00:45:59 +0530

    updated blog schema for articles

--- models/blog.js
+++ models/blog.js
@@ -1,6 +1,75 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-// mongoose.set('strictQuery', false);
+// // mongoose.set('strictQuery', false);
+
+// const blogSchema = new mongoose.Schema({
+//   title: {
+//     type: String,
+//     required: true,
+//   },
+//   author: String,
+//   url: {
+//     type: String,
+//   },
+//   content:{
+//     type:String
+//   },
+//   likes: [{
+//     type: mongoose.Schema.Types.ObjectId,
+//     ref: 'User',
+//   }],
+//   user: {
+//     type: mongoose.Schema.Types.ObjectId,
+//     ref: 'User',
+//   },
+//   comments: [{
+//     user: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: 'User',
+//     },
+//     comment: {
+//       type: String,
+//       required: true,
+//       minlength: 3
+//     },
+//     replies: [{
+//       user: {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: 'User',
+//       },
+//       reply: {
+//         type: String,
+//         required: true,
+//         minlength: 3
+//       },
+//       nestedReplies: [{
+//         user: {
+//           type: mongoose.Schema.Types.ObjectId,
+//           ref: 'User',
+//         },
+//         reply: {
+//           type: String,
+//           required: true,
+//           minlength: 3
+//         }
+//       }]
+//     }]
+//   }]
+// });
+
+// blogSchema.set('toJSON', {
+//   transform: (document, returnedObject) => {
+//     returnedObject.id = returnedObject._id.toString();
+//     delete returnedObject._id;
+//     delete returnedObject.__v;
+//   }
+// });
+
+// const Blog = mongoose.model('Blog', blogSchema);
+
+// module.exports = Blog;
+
+const mongoose = require('mongoose');
 
 const blogSchema = new mongoose.Schema({
   title: {
@@ -10,8 +79,14 @@ const blogSchema = new mongoose.Schema({
   author: String,
   url: {
     type: String,
+  },
+  content: {
+    type: String,
     required: true,
   },
+  images: [{
+    type: String,
+  }],
   likes: [{
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
@@ -52,7 +127,12 @@ const blogSchema = new mongoose.Schema({
         }
       }]
     }]
-  }]
+  }],
+  type: {
+    type: String,
+    enum: ['post', 'article'],
+    required: true,
+  }
 });
 
 blogSchema.set('toJSON', {
